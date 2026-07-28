02ee0b61b43bcb315424a78c1db2ac6e2deaa94a
JoaoDaviRibeiro <joao.davi.leite@outlook.com>
2026-07-28 17:19:29 -0300

    Agora vai

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { resolve } from 'path'
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'build',
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
